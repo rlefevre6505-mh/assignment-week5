07bd4aafef2978579bcd59253cd73005b6b66057
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2025-12-16 16:17:07 +0000

    feat: add disclaimer pop up

--- client/src/app.js
+++ client/src/app.js
@@ -40,33 +40,46 @@ function handleEaterySubmit(event) {
   });
   form.reset();
 }
-const map = L.map("map").setView([52.62963764444887, 1.30158956384622], 13);
+// const map = L.map("map").setView([52.62963764444887, 1.30158956384622], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
 
-async function getRestaurantData() {
-  const response = await fetch("http://localhost:8080/eateries");
-  const restaurantData = await response.json();
-  return restaurantData;
-}
+// async function getRestaurantData() {
+//   const response = await fetch("http://localhost:8080/eateries");
+//   const restaurantData = await response.json();
+//   return restaurantData;
+// }
 
-const restaurantData = await getRestaurantData();
-console.log(restaurantData);
+// const restaurantData = await getRestaurantData();
+// console.log(restaurantData);
 
-async function renderMarkers() {
-  const resData = await getRestaurantData();
-  createMapMarkers(resData);
-}
+// async function renderMarkers() {
+//   const resData = await getRestaurantData();
+//   createMapMarkers(resData);
+// }
 
-renderMarkers();
+// renderMarkers();
 
-function createMapMarkers(data) {
-  for (let i = 0; i < data.length; i++) {
-    let markerCoords = [data[i].location_lat, data[i].location_long];
-    let mapMarkers = L.marker(markerCoords).addTo(map);
-  }
-}
+// function createMapMarkers(data) {
+//   for (let i = 0; i < data.length; i++) {
+//     let markerCoords = [data[i].location_lat, data[i].location_long];
+//     let mapMarkers = L.marker(markerCoords).addTo(map);
+//   }
+// }
+
+//Modal Button
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("myModal");
+  const closeButton = document.getElementById("closeModal");
+
+  modal.showModal();
+
+  closeButton.addEventListener("click", () => {
+    modal.close();
+  });
+});
